8f54e8e3cb395b82bb87f9a2246b3c8f3a15241c
Slyrox-Analytics <fragtox@web.de>
2026-02-15 23:36:42 +0100

    Update charts.py

--- components/charts.py
+++ components/charts.py
@@ -1,26 +1,29 @@
-import streamlit as st
 import streamlit.components.v1 as components
 
-def tradingview_widget(symbol="BINANCE:BTCUSDT.P", height=600):
+def tradingview_widget(symbol="BINANCE:BTCUSDT.P", height=500):
 
     html = f"""
-    <div id="tv_chart" style="width:100%;"></div>
-    <script src="https://s3.tradingview.com/tv.js"></script>
-    <script>
-    new TradingView.widget({{
-        "container_id": "tv_chart",
-        "autosize": true,
-        "symbol": "{symbol}",
-        "interval": "5",
-        "timezone": "Europe/Berlin",
-        "theme": "dark",
-        "style": "1",
-        "locale": "en",
-        "toolbar_bg": "#020409",
-        "enable_publishing": false,
-        "allow_symbol_change": false
-    }});
-    </script>
+    <div style="width:100%;">
+        <div id="tv_chart" style="width:100%; height:{height}px;"></div>
+
+        <script src="https://s3.tradingview.com/tv.js"></script>
+        <script>
+        new TradingView.widget({{
+            "container_id": "tv_chart",
+            "width": "100%",
+            "height": {height},
+            "symbol": "{symbol}",
+            "interval": "5",
+            "timezone": "Europe/Berlin",
+            "theme": "dark",
+            "style": "1",
+            "locale": "en",
+            "toolbar_bg": "#020409",
+            "enable_publishing": false,
+            "allow_symbol_change": false
+        }});
+        </script>
+    </div>
     """
 
-    components.html(html, height=height, scrolling=False)
+    components.html(html, height=height)
